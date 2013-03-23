5e57786de89ce138f57f4454b091476b388b6888
Sydney Tomlinson <sydney.tomlinson@gmail.com>
2013-03-22 19:56:53 -0600

    Adding Rails boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReadIt::Application.config.secret_token = 'c0aa7796d1216e5585eb7ef6c817644b757228858694ca3190345fc6b7686c518f142725975607f5af619c7ab8985f2eaaa660ad581217e726e40d788537d74b'
