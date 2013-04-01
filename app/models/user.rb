3f68a29c9c6e559a2f26e5beebc36e6ab0b921c7
Sydney Tomlinson <sydney.tomlinson@gmail.com>
2013-03-31 21:27:48 -0600

    Adding role association to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :password, :password_confirmation, :image, :first_name, :last_name
+  attr_accessible :name, :password, :password_confirmation, :image,
+                  :first_name, :last_name
   validates :name, presence: true, uniqueness: true
   has_secure_password
 
@@ -8,8 +9,15 @@ class User < ActiveRecord::Base
 
   has_many :books
 
+  ROLES = %w[generic admin]
+
+  def role_symbols
+    [role.to_sym]
+  end
+
   after_destroy :ensure_an_admin_remains
   private
+
   	def ensure_an_admin_remains
   		if User.count.zero?
   			raise "Can't delete last admin"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130401020610) do
+ActiveRecord::Schema.define(:version => 20130401032024) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20130401020610) do
     t.datetime "image_updated_at"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "role"
   end
 
 end
