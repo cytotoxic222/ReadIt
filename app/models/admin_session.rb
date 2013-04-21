6e6f69f6a7a3fd8a8a79b59d068167aa8b71d2e2
Sydney Tomlinson <sydney.tomlinson@gmail.com>
2013-04-21 13:07:41 -0600

    More authlogic stuff

--- app/models/admin_session.rb
+++ app/models/admin_session.rb
@@ -1,6 +1,5 @@
 class AdminSession < Authlogic::Session::Base
   # attr_accessible :title, :body
-  acts_as_authentic
    def to_key 
     new_record? ? nil : [ self.send(self.class.primary_key) ] 
    end 
